6a0435831d4787aaa0cde4c936a99c41269debb0
RRx03 <romistargames@gmail.com>
2023-08-18 11:31:45 +0200

    Initial Commit

---
+++ TIPE_PerlinNoiseUpgraded_1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TIPE_PerlinNoiseUpgraded_1
+//
+//  Created by Roman Roux on 18/08/2023.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
